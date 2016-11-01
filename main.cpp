8b16433ea02c46788b158fbd835648700ad081a8
Shubham Bhattar <shubhambhattar@users.noreply.github.com>
2016-11-01 13:53:50 +0530

    Made the code sensible

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    cout<<"size";
-    int n,key;
-    bool flag=false;
-    cin>>n;
+    int n, key;
+	bool flag = false;
+	cout << "Enter the size of the array: ";
+    cin >> n;
     int arr[n];
-    3
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    1
-    cin>>key;
-    for(int i=0;i<n;i++){
-        if(key==arr[i]){
-            cout<<key<<" found at Position "<<i+1<<endl;
-            flag=true;
+	cout << "Enter " << n << " element(s): " << endl;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+	cout << "Enter the element to be searched: ";
+    cin >> key;
+    for(int i = 0; i < n; i++) {
+        if(key == arr[i]) {
+            cout << key << " found at position "<< i+1 <<endl;
+            flag = true;
         }
     }
     if(!flag)
-        cout<<key<<" not found.";
+        cout << key << " not found." << endl;
     return 0;
 }
